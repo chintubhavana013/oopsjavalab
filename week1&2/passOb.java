85e0216225a48576ffb21482ee741fec8759e2e0
chintubhavana013 <chintubhavana13@gmail.com>
2026-02-18 12:19:25 +0530

    Add files via upload

---
+++ week1&2/passOb.java
@@ -0,0 +1,29 @@
+class  test
+{
+	int a, b;
+	test(int i,int j){
+		a=i;
+	    b=j;
+	}
+		boolean equals(test o){
+		if (o.a==a && o.b==b)
+		{
+			return true;
+		}
+		else 
+			{
+			return false;
+		}
+}
+}
+class passOb
+{
+	public static void main(String[] args) 
+	{
+		test ob1=new test(100,22);
+		test ob2=new test(100,22);
+		test ob3=new test(-1,-1);
+		System.out.println(ob1.equals(ob2));
+		System.out.println(ob1.equals(ob3));
+	}
+}
